8f5a8a3fd052aed53e23da8678c36b66798874b9
Isaac Moretão <isaachanam@gmail.com>
2025-08-04 09:11:34 -0300

    remove/BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import { ThemeProvider } from "./Context/ThemeContext.tsx";
 import "./Stylles/index.css";
 import { ProductProvider } from "./Context/DataContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient()
 
@@ -20,9 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <ProductProvider>
           <PointsProvider>
             <ThemeProvider>
-              <BrowserRouter>
                 <App />
-              </BrowserRouter>
             </ThemeProvider>
           </PointsProvider>
         </ProductProvider>
