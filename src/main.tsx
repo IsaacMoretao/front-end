3410b68eb6a5d432aef0c31308782cd6df401392
Isaac Moretão <isaachanam@gmail.com>
2025-08-04 09:04:35 -0300

    Add/BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient()
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
